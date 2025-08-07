60cdb58bfe740cd7d1d0620938fca4467612a7c5
Leon Zaczek <zaczek.leon08@gmail.com>
2025-08-08 00:07:27 +0200

    I returned from vacation so I have time to code something. Today I crashed this game becouse I changed my mind and decided shoot bullets in only one direction. Moving and shooting bullets stopped working. A lot of work to do.

--- SpaceX/Pages/Game.razor.cs
+++ SpaceX/Pages/Game.razor.cs
@@ -10,10 +10,12 @@ namespace SpaceX.Pages
     public partial class Game
     {
         private Canvas2DContext _context;
-
         protected BECanvasComponent _canvasReference;
-
+        private Canvas2DContext _bulletContext;
+        protected BECanvasComponent _bulletCanvasReference;
         private ElementReference CanvaContainer;
+        private ElementReference BulletsContainer;
+        
         private int _canvasWidth = 300;
         private int _canvasHeight = 400;
         private List<Bullet> _bullets = new List<Bullet>();
@@ -28,33 +30,43 @@ namespace SpaceX.Pages
         };
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
+            Console.WriteLine("doszlo tutaj");
             if (firstRender)
             {
+                Console.WriteLine("to jest prawda");
                 _canvasHeight = await JS.InvokeAsync<int>("getScreenHeight");
                 _canvasWidth = await JS.InvokeAsync<int>("getScreenWidth");
+                StateHasChanged();
                 await JS.InvokeVoidAsync("focusElement", CanvaContainer); 
                 this._context = await this._canvasReference.CreateCanvas2DAsync();
                 await this._context.SetFillStyleAsync("green");
                 await this._context.FillRectAsync(myShip.XPosition, myShip.YPosition, myShip.ShipWidth, myShip.ShipHeight);
+                StateHasChanged();
             }
         }
         protected async Task Move(KeyboardEventArgs e)
         {
+            Console.WriteLine("time to move!");
             switch (e.Key)
             {
                 case "ArrowUp":
                     myShip.MoveUp(30);
+                    Console.WriteLine("up");
                     break;
                 case "ArrowDown":
                     myShip.MoveDown(30);
+                    Console.WriteLine("down");
                     break;
                 case "ArrowLeft":
                     myShip.MoveLeft(30);
+                    Console.WriteLine("left");
                     break;
                 case "ArrowRight":
                     myShip.MoveRight(30);
+                    Console.WriteLine("right");
                     break;
                 default:
+                    Console.WriteLine("kupa");
                     return;
             }
 
@@ -67,19 +79,22 @@ namespace SpaceX.Pages
         }
         private async Task SendNewBullet(MouseEventArgs e)
         {
-            int xposition = Convert.ToInt32(e.OffsetX);
-            int yposition = Convert.ToInt32(e.OffsetY);
-            double a = (myShip.YPosition - yposition) / (myShip.XPosition - xposition);
-            double b = yposition - a * xposition;
-            
-
-
-            await this._context.SetStrokeStyleAsync("red");
-            await this._context.SetLineWidthAsync(5);
-            await this._context.BeginPathAsync();
-            await this._context.MoveToAsync(myShip.XPosition, myShip.YPosition);
-            await this._context.LineToAsync(xposition, yposition);
-            await this._context.StrokeAsync();
+            Console.WriteLine("wysylam bulleta");
+            Bullet newBullet = new Bullet
+            {
+                ID = _bullets.Count,
+                XPosition = myShip.XPosition + (myShip.ShipWidth / 2),
+                YPosition = 0
+            };
+            Console.WriteLine("bullet zrobiony");
+            /*
+            await this._bulletContext.SetStrokeStyleAsync("red");
+            await this._bulletContext.SetLineWidthAsync(5);
+            await this._bulletContext.BeginPathAsync();
+            await this._bulletContext.MoveToAsync(myShip.XPosition + (myShip.ShipWidth / 2), myShip.YPosition);
+            await this._bulletContext.LineToAsync(newBullet.XPosition, newBullet.YPosition);
+            await this._bulletContext.StrokeAsync();
+            */
 
         }
     }
